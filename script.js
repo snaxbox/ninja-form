2fd64e828057ecf91bc32f5f21fde37f1c603d05
snaxbox <kekeenglish2018@gmail.com>
2020-10-26 12:45:08 +0800

    js functions added for the modal- on click of the submit button

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 
 const myForm = document.getElementById("my-form");
+const crossBtn = document.querySelector(".cross");
+const overlay = document.querySelector(".modal-overlay");
+const modalContent= document.querySelector(".modal-content");
+
 
 myForm.addEventListener("submit", function(e){
+    
     e.preventDefault();
-    console.log("helloworld");
-    myForm.reset();
-    alert("报名表已发送")
+    // overlay.classList.add(".modal-active");
+    overlay.style.display ="block";
+    // console.log("helloworld");
+    // myForm.reset();
+    // alert("报名表已发送")
 });
+
+crossBtn.addEventListener("click", function(){
+    
+    // myForm.reset();
+
+    modalContent.classList.add("close-animation");
+
+    setTimeout(closeAnimation, 400);
+});
+
+overlay.addEventListener("click", function(){
+    
+    modalContent.classList.add("close-animation");
+
+    setTimeout(closeAnimation, 400);
+});
+
+
+function closeAnimation(){
+    myForm.reset();
+    overlay.style.display ="none";
+    location.reload();
+}
